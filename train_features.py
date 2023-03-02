3300236b0c933fd131437b54c7362c814d01a584
Ewen MICHEL <ewenmichel@Ewens-MacBook-Pro.local>
2023-03-02 21:15:35 +0100

    merge conflict

--- train_features.py
+++ train_features.py
@@ -306,7 +306,6 @@ if __name__ == "__main__":
     import datetime
 
     now = datetime.datetime.now().strftime(r"%y_%m_%d_%H")
-    audio_df = pd.read_csv("audio_features_test.csv")
     audio_df = pd.read_csv("audio_video_features_test.csv")
     model_l = [
         models.AudioGRU(),
